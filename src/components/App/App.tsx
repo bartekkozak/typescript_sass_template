c25c2c332a0d70afa0f252c0ffdf3998d8c0d5df
bartek <bartekkozak3@gmail.com>
2019-04-16 22:27:29 +0200

    first commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.scss";
+
+const App = (): JSX.Element => (
+  <div className="App">
+    <header className="App-header">REACT TSX</header>
+  </div>
+);
+
+export default App;
